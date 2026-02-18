b4ac16d744f3b13deb6394092ef838b0464e332d
DHARANISH\itsme <dharania81@gmail.com>
2026-02-18 13:54:18 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -700,7 +700,7 @@ const App = () => {
                       </div>
                       
                       <div className="contact-social">
-                        <a href="#" className="social-link">
+                        <a href="https://github.com/dharanish1328" className="social-link">
                           <div className="social-icon">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
                               <path d="M12 2C6.477 2 2 6.477 2 12C2 17.523 6.477 22 12 22C17.523 22 22 17.523 22 12C22 6.477 17.523 2 12 2Z" stroke="currentColor" strokeWidth="1.5"/>
@@ -709,7 +709,7 @@ const App = () => {
                           </div>
                           <span>GitHub</span>
                         </a>
-                        <a href="#" className="social-link">
+                        <a href="https://www.linkedin.com/in/dharanish-a-4b72b226b" className="social-link">
                           <div className="social-icon">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
                               <path d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z" stroke="currentColor" strokeWidth="1.5"/>
@@ -832,12 +832,12 @@ const App = () => {
       </div>
       
       <div className="simple-footer-social">
-        <a href="#" className="simple-social-link" aria-label="GitHub">
+        <a href="https://github.com/dharanish1328" className="simple-social-link" aria-label="GitHub">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
           </svg>
         </a>
-        <a href="#" className="simple-social-link" aria-label="LinkedIn">
+        <a href="https://www.linkedin.com/in/dharanish-a-4b72b226b" className="simple-social-link" aria-label="LinkedIn">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
             <rect x="2" y="9" width="4" height="12"/>
